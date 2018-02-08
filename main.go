3ab4fb453dc982af590acbed3c525a7d87eb3228
Max Gonzih <gonzih@gmail.com>
2018-02-07 19:38:12 -0500

    Update

--- main.go
+++ main.go
@@ -1,15 +1,86 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+
+	"github.com/Gonzih/screensaver-off-go/icons"
 	"github.com/getlantern/systray"
 )
+
+var (
+	disableScreensaver     bool
+	disableScreensaverLock sync.RWMutex
+)
+
+func tryToDisable() {
+	disableScreensaverLock.RLock()
+	defer disableScreensaverLock.RUnlock()
+
+	if disableScreensaver {
+		log.Println("Trying to disable xscreensaver")
+		cmd := exec.Command("/usr/bin/xscreensaver-command", "-deactivate")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			log.Printf("Error executing xscreensaver-command: %s\n", err)
+		}
+	}
+}
+
+func screensaverLoop() {
+	for {
+		tryToDisable()
+		time.Sleep(time.Second)
+	}
+}
 
 func main() {
 	systray.Run(onReady, onExit)
 }
 
 func onReady() {
-	systray.SetTitle("Disable screensaver")
+	systray.SetIcon(icons.IconFull)
+	systray.SetTitle("Disable xscreensaver")
+	systray.SetTooltip("Disable xscreensaver")
+	mToggle := systray.AddMenuItem("Disable", "Disable")
+
+	go func() {
+		for {
+			<-mToggle.ClickedCh
+
+			disableScreensaverLock.Lock()
+			disableScreensaver = !disableScreensaver
+			disableScreensaverLock.Unlock()
+
+			disableScreensaverLock.RLock()
+
+			if disableScreensaver {
+				mToggle.Check()
+				mToggle.SetTitle("Enable")
+				systray.SetIcon(icons.IconEmpty)
+			} else {
+				mToggle.Uncheck()
+				mToggle.SetTitle("Disable")
+				systray.SetIcon(icons.IconFull)
+			}
+
+			disableScreensaverLock.RUnlock()
+		}
+	}()
+
+	mQuit := systray.AddMenuItem("Quit", "Quit screensaver-off")
+
+	go func() {
+		<-mQuit.ClickedCh
+		os.Exit(0)
+	}()
+
+	log.Println("Ready!")
 }
 
 func onExit() {
